4b33fbd3d522bfc2ece2d9716d9e2820f8eb2990
Helder Santos <heldersantos@bemol.com.br>
2020-11-17 23:15:33 -0400

    relacionando o usuario como foreignkey
    Signed-off-by: Helder Santos <heldersantos@bemol.com.br>

--- myapp/models.py
+++ myapp/models.py
@@ -1,17 +1,26 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 # DJANGO ORM - Object Relational Mapper
 
+# models.PROTECT -> não permite que o usuario seja excluido se tiver post
+# models.CASCADE -> remove todos os posts quando o usuario é excluido
+# models.SET_NULL -> inseri null em todos os post que o usuario foi excluido,
+# o campo precisa permitir que seja colocado null
+
+# campo subtitle facultativo, blank -> faz com que seja possivel enviar vazio pelo painel-adm-
+
 
 class Post(models.Model):
     title = models.CharField(max_length=255, verbose_name="Titulo")
-    # campo subtitle facultativo, blank -> faz com que seja possivel enviar vazio pelo painel-adm-
     subtitle = models.CharField(
         max_length=255, null=True, blank=True, verbose_name="Subtitulo"
     )
     content = models.TextField(verbose_name="Corpo do artigo")
-    user = models.ForeignKey()
+    user = models.ForeignKey(
+        User, on_delete=models.PROTECT, null=True, verbose_name="Autor"
+    )
 
     # aqui ele alterou o nome que é apresentado no painel-adm
     class Meta:
@@ -21,3 +30,8 @@ class Post(models.Model):
     # aqui ele retorna o titulo do post ao inves de Post.object.id(1)
     def __str__(self):
         return self.title
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField(null=True, blank=True)
